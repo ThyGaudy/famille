2387cc6a0c4977238021a0a4e3cf7ac8e26b0d3c
ThyGaudy <amiralletuc@gmail.com>
2026-07-05 23:55:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker minimaliste : cache l'app pour qu'elle marche hors-ligne.
 // Change la version ci-dessous quand tu modifies l'app pour forcer un refresh.
-const CACHE_NAME = 'famille-v9';
+const CACHE_NAME = 'famille-v10';
 const ASSETS = [
   './',
   './index.html',
